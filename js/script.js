91b8efafd094d43a888c62df8721a13d35096928
Mariia <milonigame1@gmail.com>
2022-12-15 15:35:45 +0300

    all

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+const category='toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user='Ivan';
+alert(`hello,${user}`);
+alert('ghj');
+// hjkhk
+// kljkl
+alert(6);
