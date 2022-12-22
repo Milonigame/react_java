9f9b3a9f1a10a2d20a24655642e59423661025c3
Mariia <milonigame1@gmail.com>
2022-12-22 16:18:43 +0300

    ready task vagon

--- task_kub.js
+++ task_kub.js
@@ -16,40 +16,54 @@ calculateVolumeAndArea(5);
 
 // Место для второй задачи
 function getCoupeNumber(s) {
-    if (typeof(s) !== 'number' || !Number.isInteger(s)){
+    if (typeof(s) !== 'number' ||s<0|| !Number.isInteger(s)){
         return "Ошибка. Проверьте правильность введенного номера места";
     }
-    else if (s <=0||s>36){
+    else if (s ===0||s>36){
         return "Таких мест в вагоне не существует";
     }
    else if ( s<=4){
-    return '=> 1';
+    return 1;
    }
    else if ( s>=5&&s<=8){
-    return '=> 2';
+    return 2;
    }
    else if ( s>=9&&s<=12){
-    return '=> 3';
+    return 3;
    }
    else if ( s>=13&&s<=16){
-    return '=> 4';
+    return 4;
    }
    else if ( s>=17&&s<=20){
-    return '=> 5';
+    return 5;
    }
    else if ( s>=21&&s<=24){
-    return '=> 6';
+    return  6;
    }
    else if ( s>=25&&s<=28){
-    return '=> 7';
+    return  7;
    }
    else if ( s>=29&&s<=32){
-    return '=> 8';
+    return 8;
    }
-   else if ( s>=34&&s<=36){
-    return '=> 9';
+   else if ( s>=33&&s<=36){
+    return 9;
    }
    
 }
 
-getCoupeNumber(36);
+getCoupeNumber(33);
+// function getCoupeNumber(seatNumber) {
+//     if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+//         return "Ошибка. Проверьте правильность введенного номера места";
+//     }
+
+//     if (seatNumber === 0 || seatNumber > 36) {
+//         return "Таких мест в вагоне не существует";
+//     }
+
+//     return Math.ceil(seatNumber / 4);
+//     // тут очень много вариантов решения, но выбрал один из элегантных :)
+// }
+
+// getCoupeNumber(33);
