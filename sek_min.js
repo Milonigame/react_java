1d6fd0b78e662b75ce053a3d90b6072818fee948
Mariia <milonigame1@gmail.com>
2022-12-22 18:20:12 +0300

    all ready first task

--- sek_min.js
+++ sek_min.js
@@ -1,15 +1,29 @@
-function timeCheck(sec){
+function getTimeFromMinutes(sec){
+if (typeof(sec) !== 'number' || sec < 0 || !Number.isInteger(sec)) {
+        return "Ошибка, проверьте данные";
+}
 let hourCalc;
 let minutCalc;
-
+let hourStr="";
 hourCalc=Math.floor(sec / 60);
-minutCalc=Math.floor(sec%3600)%60;
-
-
- return `Это ${hourCalc} часа и ${minutCalc} минут `;
-
-
+minutCalc=Math.floor(sec%60);
+switch (hourCalc) {
+    case 0: 
+        hourStr = 'часов';
+        break;
+    case 1:
+        hourStr = 'час';
+        break;
+    case 2:
+    case 3:
+    case 4:
+        hourStr = 'часа';
+        break;
+    default:
+        hourStr = 'часов';
 }
-timeCheck(150);
+ return `Это ${hourCalc} ${hourStr} и ${minutCalc} минут `;
+}
+getTimeFromMinutes(150);
 
 
