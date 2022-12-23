8edd95e98181f4433501a32c6dc9ac7da20f86c0
Mariia <milonigame1@gmail.com>
2022-12-23 15:17:35 +0300

    ready fibbonacci

--- sek_min.js
+++ sek_min.js
@@ -6,7 +6,7 @@ let hourCalc;
 let minutCalc;
 let hourStr="";
 hourCalc=Math.floor(sec / 60);
-minutCalc=Math.floor(sec%60);
+minutCalc=sec%60;
 switch (hourCalc) {
     case 0: 
         hourStr = 'часов';
@@ -24,6 +24,52 @@ switch (hourCalc) {
 }
  return `Это ${hourCalc} ${hourStr} и ${minutCalc} минут `;
 }
-getTimeFromMinutes(150);
+
+console.log(getTimeFromMinutes(150)); 
 
 
+
+function getTimeFromMinutes(minutesTotal) {
+    if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
+        return "Ошибка, проверьте данные";
+    }
+
+    const hours = Math.floor(minutesTotal / 60);
+    const minutes = minutesTotal % 60;
+
+    let hoursStr = '';
+
+    switch (hours) {
+        case 0: 
+            hoursStr = 'часов';
+            break;
+        case 1:
+            hoursStr = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            hoursStr = 'часа';
+            break;
+        default:
+            hoursStr = 'часов';
+    }
+
+    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
+}
+
+getTimeFromMinutes(180)
+
+function findMaxNumber(a,b,c,d){
+    if (typeof(a) !== 'number' ||
+    typeof(b) !== 'number' ||
+    typeof(c) !== 'number' ||
+    typeof(d) !== 'number') {
+    return 0;}
+else{
+    let max;
+    max=Math.max(a,b,c,d);
+    return max;
+}
+}
+findMaxNumber(1, 5, '6.6', 11);
